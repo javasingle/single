94bfeb1f128bc75390bf748042f04f557ffa9c3f
李振斌 <li15837125183@aliyun.com>
2020-01-11 14:20:22 +0800

    添加子模块singleadmin

---
+++ sing-admin/src/main/java/com/single/singadmin/SingAdminApplication.java
@@ -0,0 +1,13 @@
+package com.single.singadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SingAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SingAdminApplication.class, args);
+    }
+
+}
---
+++ sing-admin/src/test/java/com/single/singadmin/SingAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.single.singadmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SingAdminApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
